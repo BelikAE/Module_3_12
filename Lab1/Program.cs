1f4e372908e8416ff88693a5f0aa1c4def11c884
BelikAE <BelikAE@HOME-PC>
2024-07-19 23:12:37 +1000

    Выдача

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,29 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Введите радиус");
+                double r = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату центра окружности x0");
+                double x0 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату центра окружности y0");
+                double y0 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Длина окружности: {Сircle.GetLength(r)}");
+                Console.WriteLine($"Площадь круга: {Сircle.GetSquare(r)}");
+                Сircle.СontainsPoint(r, x0, y0);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+              
+
+
+
+
         }
     }
 }
---
+++ Lab1/Сircle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    static class Сircle
+    {
+
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        static public void СontainsPoint(double r, double x0, double y0)
+        {
+            Console.WriteLine("Введите координату точки x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату точки y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double c = Math.Sqrt(Math.Pow(x0 - x, 2) + Math.Pow(y0 - y, 2));
+            if (c <= r)
+            {
+                Console.WriteLine("Точка принадлежит окружности");
+
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит окружности");
+            }
+           
+                
+        }
+
+    }
+}
